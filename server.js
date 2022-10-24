312b895b4794b26c58baa2f86e0f8ac089963ccc
Sarah Duperoux <Duperouxsarah@gmail.com>
2022-10-24 16:08:17 -0400

    added servere functionality with port and mongodb

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001
+const routes = require('./routes')
+const db = require('./db')
 
 const app = express()
+
+//middleware
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
