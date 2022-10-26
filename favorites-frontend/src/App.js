ef9fb5810ca2bbbe04f01e0a5b83ab522059b66d
Sarah Duperoux <Duperouxsarah@gmail.com>
2022-10-26 14:40:42 -0400

    added form  and input for favs

--- favorites-frontend/src/App.js
+++ favorites-frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css'
 import axios from 'axios'
 import { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Home'
 // import { useState, useEffect } from 'react'
 
 function App() {
+  // const [Favorite, updateFavorite] = useState([])
+  // const [formState, setFormState] = useState({
+  //   artist: '',
+  //   image: '',
+  //   lyrics: '',
+  //   albums: ''
+  // })
+
   useEffect(() => {
     const apiCall = async () => {
       let response = await axios.get('localhost:3001/')
@@ -14,9 +24,23 @@ function App() {
 
   return (
     <div className="App">
-      <header></header>
+      <header>
+        {/* <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes> */}
+      </header>
       <h1>Favorite Artist Directory</h1>
-      <main></main>
+      <form>
+        <label id="artist">Artist:</label>
+        <input></input>
+        <label id="image">Image:</label>
+        <input></input>
+        <label id="lyrics">Lyrics:</label>
+        <input></input>
+        <label id="albums"># of Albums:</label>
+        <input></input>
+      </form>
+      <div></div>
     </div>
   )
 }
