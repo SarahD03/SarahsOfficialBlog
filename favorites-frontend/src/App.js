33de612b56d87cd931865ee57a6ad3ec4ae321ec
Sarah Duperoux <Duperouxsarah@gmail.com>
2022-10-26 11:11:23 -0400

    added schema information and app.js functions

--- favorites-frontend/src/App.js
+++ favorites-frontend/src/App.js
@@ -1,10 +1,22 @@
 import './App.css'
+import axios from 'axios'
+import { useEffect } from 'react'
 // import { useState, useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    const apiCall = async () => {
+      let response = await axios.get('localhost:3001/')
+      return response
+    }
+    console.log(apiCall())
+  }, [])
+
   return (
     <div className="App">
+      <header></header>
       <h1>Favorite Artist Directory</h1>
+      <main></main>
     </div>
   )
 }
