7ae7cc69ebca508147441716d44a98d0825df3ff
Sarah Duperoux <Duperouxsarah@gmail.com>
2022-10-26 15:46:19 -0400

    added more app functions and fixed errors

--- favorites-frontend/src/App.js
+++ favorites-frontend/src/App.js
@@ -1,26 +1,44 @@
 import './App.css'
 import axios from 'axios'
-import { useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './Home'
-// import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
+// import { Routes, Route } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 
 function App() {
-  // const [Favorite, updateFavorite] = useState([])
-  // const [formState, setFormState] = useState({
-  //   artist: '',
-  //   image: '',
-  //   lyrics: '',
-  //   albums: ''
-  // })
+  const [favorites, updateFavorites] = useState([])
+  const [formState, setFormState] = useState({
+    artist: '',
+    image: '',
+    lyrics: '',
+    albums: ''
+  })
 
   useEffect(() => {
     const apiCall = async () => {
-      let response = await axios.get('localhost:3001/')
+      let response = await axios.get('localhost:3001/favorites')
       return response
     }
     console.log(apiCall())
   }, [])
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    console.log(formState)
+    let newFavorites = await axios
+      .post('http://localhost:3001/', formState)
+      .then((response) => {
+        return response
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    updateFavorites([...favorites, newFavorites.data])
+    setFormState({ artist: '', image: '', lyrics: '', albums: '' })
+    console.log(newFavorites.data)
+  }
 
   return (
     <div className="App">
@@ -39,6 +57,7 @@ function App() {
         <input></input>
         <label id="albums"># of Albums:</label>
         <input></input>
+        <button type="submit">Add Artist</button>
       </form>
       <div></div>
     </div>
--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const cors = require('cors')
 const PORT = process.env.PORT || 3001
 const routes = require('./routes')
 const db = require('./db')
+const { Favorite, Comments } = require('./models')
 
 const app = express()
 //middleware
 app.use(express.json())
 app.use(cors())
+// app.use('/api', routes)
 
 app.get('/', (req, res) => {
   res.send({ msg: 'This route is being hit' })
